a2c1ee8c1db0556659999adf134b4d351d1867b3
simonmarcos <marcoseduardosimon@gmail.com>
2022-12-21 04:20:05 -0300

    Created a custom mapper to concatenate the path and extension

--- src/main/java/com/test/challenge/challenge/service/marvel/mapper/CharacterMarvelMapper.java
+++ src/main/java/com/test/challenge/challenge/service/marvel/mapper/CharacterMarvelMapper.java
@@ -2,9 +2,18 @@ package com.test.challenge.challenge.service.marvel.mapper;
 
 import com.test.challenge.challenge.service.dto.CharacterDTO;
 import com.test.challenge.challenge.service.marvel.model.CharacterMarvel;
+import com.test.challenge.challenge.service.marvel.model.ThumbnailMarvel;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface CharacterMarvelMapper {
+    @Mapping(source = "thumbnailMarvel", target = "thumbnail", qualifiedByName = "concatThumbnail")
     CharacterDTO characterMarvelToCharacterDTO(CharacterMarvel characterMarvel);
+
+    @Named("concatThumbnail")
+    static String concatThumbnail(ThumbnailMarvel thumbnailMarvel) {
+        return thumbnailMarvel.getPath() + "." + thumbnailMarvel.getExtension();
+    }
 }
