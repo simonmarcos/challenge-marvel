73ed674bf67582d5e86939927633b958a79e6cca
simonmarcos <marcoseduardosimon@gmail.com>
2022-12-22 16:24:29 -0300

    A model was created to generate a json format response when logging in

--- src/main/java/com/test/challenge/challenge/security/jwt/JWTAuthenticationFilter.java
+++ src/main/java/com/test/challenge/challenge/security/jwt/JWTAuthenticationFilter.java
@@ -1,8 +1,10 @@
 package com.test.challenge.challenge.security.jwt;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.test.challenge.challenge.security.AuthCredentialsDTO;
 import com.test.challenge.challenge.security.UserDetailsImpl;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -13,10 +15,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.ZonedDateTime;
 import java.util.Collections;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
@@ -28,11 +30,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             ioException.printStackTrace();
         }
 
-        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-                authCredentials.getEmail(),
-                authCredentials.getPassword(),
-                Collections.emptyList()
-        );
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(authCredentials.getEmail(), authCredentials.getPassword(), Collections.emptyList());
 
         return getAuthenticationManager().authenticate(usernamePasswordAuthenticationToken);
     }
@@ -42,9 +40,21 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         UserDetailsImpl userDetails = (UserDetailsImpl) authResult.getPrincipal();
         String token = TokenUtils.createToken(userDetails.getName(), userDetails.getUsername());
 
-        response.addHeader("Authorization", "Bearer " + token);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(getResponseBodyInString(token));
         response.getWriter().flush();
 
         super.successfulAuthentication(request, response, chain, authResult);
+    }
+
+    private String getResponseBodyInString(String token) throws JsonProcessingException {
+        CustomResponse customResponse = new CustomResponse();
+        customResponse.setToken(token);
+        customResponse.setStatus(HttpStatus.OK);
+        customResponse.setDateTime(ZonedDateTime.now().toString());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(customResponse);
     }
 }
